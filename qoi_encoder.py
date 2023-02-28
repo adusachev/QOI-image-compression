d153fb42545a341217e75b977e02a01a6d601a1e
adusachev <usachev.alexe@gmail.com>
2023-02-28 17:37:04 +0300

    init commit

---
+++ qoi_encoder.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/test_example.py
@@ -0,0 +1,34 @@
+import unittest
+
+""" 
+run:
+> python -m unittest tests.test_example
+"""
+
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+
+class TestProd(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(5 * 2, 10, "Should be 10")
+
+    def test_sum_tuple(self):
+        self.assertEqual(6 * 7, 36, "Should be 36")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
+    
+    
+    
+    
