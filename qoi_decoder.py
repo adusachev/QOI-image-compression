e06c33071c3a942e3b16def3dd16689f914c89d3
adusachev <usachev.alexe@gmail.com>
2023-04-08 16:00:45 +0300

    first version of decoder

--- qoi_decoder.py
+++ qoi_decoder.py
@@ -6,8 +6,140 @@ from qoi_encoder import Pixel
 from main import read_png
 
 
+def decode_byte_part_0(byte: int, part_length: int) -> int:
+    """
+    Return right part of byte of given length
+     (i.e. slice byte[ 8 - part_length: ])
+    """
+    res = byte >> part_length
+
+    res2 = res << part_length
+
+    return (byte - res2)
 
 
+def decode_byte_part(byte: int, right_offset: int, bits_num: int) -> int:
+    """
+    Extract part of given legnth from a byte
+
+    :param byte: single byte (0-255)
+    :param right_offset: e.g. right_offset=4 if we want to extract 1011 from byte 1011xxxx 
+    :param bits_num: number of bits in desired part of byte
+    """
+    byte = byte >> right_offset  # cut right part of byte of length "right_offset"
+    res = byte >> bits_num  # cut right part of byte of length "bits_num"
+    mask = res << bits_num  # add "bits_num" zeros to the end of "res"
+    byte_part = (byte - mask) 
+    return byte_part
+
+
+
+def decode_diff_small(byte: int) -> tuple(int):
+    """
+    Extract dr, dg, db from diff small byte
+    """
+    dr = decode_byte_part(byte, right_offset=4, bits_num=2)
+    dg = decode_byte_part(byte, right_offset=2, bits_num=2)
+    db = decode_byte_part(byte, right_offset=0, bits_num=2)
+    
+    return dr, dg, db
+
+
+def decode_diff_med(byte1: int, byte2: int) -> tuple(int):
+    """
+    Extract dr, dg, db from diff med bytes
+    """
+    dg = decode_byte_part(byte1, right_offset=0, bits_num=6)
+    dr_dg = decode_byte_part(byte2, right_offset=4, bits_num=4)
+    db_dg = decode_byte_part(byte2, right_offset=0, bits_num=4)
+    
+    dr = dr_dg + dg
+    db = db_dg + dg
+    
+    return dr, dg, db
+
+
+
+def decode(filename, width, height):
+    """
+    Algorithm of QOI decoder
+    """
+    
+    with open('./data/tmp_v2.txt', 'rb') as f:
+        qoi_bytes = f.read()
+    
+    n = width * height
+    
+    R_decoded = np.zeros(n)
+    G_decoded = np.zeros(n)
+    B_decoded = np.zeros(n)
+    hash_array = [None for i in range(64)]
+    
+    m = len(encoded_img)
+    
+    pixel_counter = 0  # flatten image counter
+    byte_counter = 0  # qoi bytes counter
+    
+    while byte_counter != m:
+        
+        prev_pixel = cur_pixel
+        
+        byte = qoi_bytes[byte_counter]
+        tag = byte >> 6  # first 2 numbers of byte (e.g. ab from abxxxxxx)
+        
+        if byte == 0b11111110:
+            tag_name = 'rgb'
+            cur_pixel = Pixel(qoi_bytes[byte_counter+1],
+                              qoi_bytes[byte_counter+2], 
+                              qoi_bytes[byte_counter+3])         
+            byte_counter += 4
+            
+        elif tag == 0b11:
+            tag_name = 'run'
+            run_length = decode_byte_part(byte, right_offset=0, bits_num=6)
+            
+            for i in range(run_length):
+                R_decoded[pixel_counter + i] = prev_pixel.r
+                G_decoded[pixel_counter + i] = prev_pixel.g
+                B_decoded[pixel_counter + i] = prev_pixel.b
+            pixel_counter += run_length
+            continue
+            
+        elif tag == 0b00:
+            tag_name = 'index'
+            hash_index = decode_byte_part(byte, right_offset=0, bits_num=6)
+            cur_pixel = hash_array[hash_index]
+            byte_counter += 1
+            
+            if hash_array[hash_index] is None:
+                raise ValueError("smth wrong with hashes")
+        
+        elif tag == 0b01:
+            tag_name = 'diff_small'
+            dr, dg, db = decode_diff_small(byte)
+            cur_pixel = Pixel(prev_pixel.r + dr, prev_pixel.g + dg, prev_pixel.b + db)
+            byte_counter += 1
+            
+        elif tag == 0b10:
+            tag_name = 'diff_med'
+            next_byte = qoi_bytes[i+1]
+            dr, dg, db = decode_diff_med(byte, next_byte)
+            cur_pixel = Pixel(prev_pixel.r + dr, prev_pixel.g + dg, prev_pixel.b + db)
+            byte_counter += 2
+
+        else:
+            raise ValueError(f"Unknown tag value: {bin(tag)} in byte {bin(byte)}")
+        
+        hash_index = int(cur_pixel.hash_value())
+        hash_array[hash_index] = cur_pixel
+        
+        R_decoded[pixel_counter] = cur_pixel.r
+        G_decoded[pixel_counter] = cur_pixel.g
+        B_decoded[pixel_counter] = cur_pixel.b
+        pixel_counter += 1
+            
+    
+    
 
 
 
--- tests/test_decoder.py
+++ tests/test_decoder.py
@@ -5,11 +5,73 @@ from main import read_png
 import numpy as np
 import os
 import pickle
+from qoi_decoder import decode_byte_part, decode_byte_part_0
 
 
-class TestDebugDecoder(unittest.TestCase):
+class TestDecoder(unittest.TestCase):
+    
+    def test_decode_byte_part_2(self):
         
-    def test1(self):
+        # run test
+        byte = 0b11001010
+        run_length = decode_byte_part(byte, right_offset=0, bits_num=6)
+        self.assertEqual(run_length, 0b001010, f"Expected {bin(0b001010)} but got {bin(run_length)}")
+        
+        # diff small test
+        byte = 0b01111001
+        dr = decode_byte_part(byte, right_offset=4, bits_num=2)
+        self.assertEqual(dr, 0b11, f"Expected {bin(0b11)} but got {bin(dr)}")
+        
+        dg = decode_byte_part(byte, right_offset=2, bits_num=2)
+        self.assertEqual(dg, 0b10, f"Expected {bin(0b10)} but got {bin(dg)}")
+        
+        db = decode_byte_part(byte, right_offset=0, bits_num=2)
+        self.assertEqual(db, 0b01, f"Expected {bin(0b01)} but got {bin(db)}")
+        
+        # diff med test (byte1)
+        byte = 0b10011011
+        dg = decode_byte_part(byte, right_offset=0, bits_num=6)
+        self.assertEqual(dg, 0b011011, f"Expected {0b011011} but got {bin(dg)}")
+        
+        # diff med test (byte2)
+        byte = 0b11111100
+        db_dg = decode_byte_part(byte, right_offset=0, bits_num=4)
+        self.assertEqual(db_dg, 0b1100, f"Expected {bin(0b1100)} but got {bin(db_dg)}")
+        
+        dr_dg = decode_byte_part(byte, right_offset=4, bits_num=4)
+        self.assertEqual(dr_dg, 0b1111, f"Expected {bin(0b1111)} but got {bin(dr_dg)}")
+    
+        
+    def test_decode_byte_part_0(self):
+        
+        # run test
+        byte = 0b11001010
+        run_length = decode_byte_part_0(byte, part_length=6)
+        self.assertEqual(run_length, 0b001010, f"Expected {bin(0b001010)} but got {bin(run_length)}")
+        
+        # diff small test
+        byte = 0b01111001
+        db = decode_byte_part_0(byte, part_length=2)
+        self.assertEqual(db, 0b01, f"Expected {bin(0b01)} but got {bin(db)}")
+        
+        # diff med test (byte1)
+        byte = 0b10011011
+        dg = decode_byte_part_0(byte, part_length=6)
+        self.assertEqual(dg, 0b011011, f"Expected {0b011011} but got {bin(dg)}")
+        
+        # diff med test (byte2)
+        byte = 0b11111100
+        db_dg = decode_byte_part_0(byte, part_length=4)
+        self.assertEqual(db_dg, 0b1100, f"Expected {bin(0b1100)} but got {bin(db_dg)}")
+        
+    
+    
+        
+        
+        
+        
+        
+    def test_debug_decoder(self):
         
         files = ['R_video', '28_pixels', 'pixel_diff', 'doge']
         
